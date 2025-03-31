e9933afec089c7ba14d1aa4ae1609ddc2fbb18ce
Phan Dinh <81685801+phanvandinh99@users.noreply.github.com>
2025-03-31 22:22:48 +0700

    Create new project

---
+++ DichVuBus/WebBus/Areas/HocSinh/HocSinhAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebBus.Areas.HocSinh
+{
+    public class HocSinhAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "HocSinh";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "HocSinh_default",
+                "HocSinh/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
