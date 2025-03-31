77aed2f6e3823aee334dfe3817e19859e6b3c3ec
Phan Dinh <81685801+phanvandinh99@users.noreply.github.com>
2025-03-31 22:41:44 +0700

    Update Models

--- DichVuBus/WebBus/Models/Models.cs
+++ DichVuBus/WebBus/Models/Models.cs
@@ -7,43 +7,43 @@ namespace WebBus.Models
     {
         [BsonId]
         public ObjectId Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
     }
 
     public class HocSinh
     {
         [BsonId]
         public ObjectId Id { get; set; }
-        public string HoTen { get; set; }
-        public string Lop { get; set; }
-        public string TuyenDuongId { get; set; }
-        public string UserId { get; set; }
+        public string hoTen { get; set; }
+        public string lop { get; set; }
+        public string tuyenDuongId { get; set; }
+        public string userId { get; set; }
     }
 
     public class XeBus
     {
         [BsonId]
         public ObjectId Id { get; set; }
-        public string BienSo { get; set; }
-        public string TrangThai { get; set; }
+        public string bienSo { get; set; }
+        public string trangThai { get; set; }
     }
 
     public class TuyenDuong
     {
         [BsonId]
         public ObjectId Id { get; set; }
-        public string TenTuyen { get; set; }
-        public string XeBusId { get; set; }
+        public string tenTuyen { get; set; }
+        public string xeBusId { get; set; }
     }
 
     public class LichTrinh
     {
         [BsonId]
         public ObjectId Id { get; set; }
-        public string TuyenDuongId { get; set; }
-        public string ThoiGian { get; set; }
-        public string Ngay { get; set; }
+        public string tuyenDuongId { get; set; }
+        public string thoiGian { get; set; }
+        public string ngay { get; set; }
     }
 }
