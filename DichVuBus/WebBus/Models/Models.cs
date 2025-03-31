329ccd6ba581028ee0f28edca773bd94997f3e91
Phan Dinh <81685801+phanvandinh99@users.noreply.github.com>
2025-03-31 22:28:17 +0700

    Update Models

---
+++ DichVuBus/WebBus/Models/Models.cs
@@ -0,0 +1,49 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebBus.Models
+{
+    public class User
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class HocSinh
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string HoTen { get; set; }
+        public string Lop { get; set; }
+        public string TuyenDuongId { get; set; }
+        public string UserId { get; set; }
+    }
+
+    public class XeBus
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string BienSo { get; set; }
+        public string TrangThai { get; set; }
+    }
+
+    public class TuyenDuong
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string TenTuyen { get; set; }
+        public string XeBusId { get; set; }
+    }
+
+    public class LichTrinh
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string TuyenDuongId { get; set; }
+        public string ThoiGian { get; set; }
+        public string Ngay { get; set; }
+    }
+}
---
+++ DichVuBus/WebBus/Models/MongoDBContext.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+
+namespace WebBus.Models
+{
+    public class MongoDBContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDBContext()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            _database = client.GetDatabase("DataDichVuBus");
+        }
+
+        public IMongoCollection<User> Users => _database.GetCollection<User>("Users");
+        public IMongoCollection<HocSinh> HocSinh => _database.GetCollection<HocSinh>("HocSinh");
+        public IMongoCollection<XeBus> XeBus => _database.GetCollection<XeBus>("XeBus");
+        public IMongoCollection<TuyenDuong> TuyenDuong => _database.GetCollection<TuyenDuong>("TuyenDuong");
+        public IMongoCollection<LichTrinh> LichTrinh => _database.GetCollection<LichTrinh>("LichTrinh");
+    }
+}
