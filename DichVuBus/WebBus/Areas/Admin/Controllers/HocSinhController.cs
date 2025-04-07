58255bb8d2ad4bb287b135eac0983d556d515cd9
Phan Dinh <81685801+phanvandinh99@users.noreply.github.com>
2025-04-07 22:05:43 +0700

    Update học sinh

--- DichVuBus/WebBus/Areas/Admin/Controllers/HocSinhController.cs
+++ DichVuBus/WebBus/Areas/Admin/Controllers/HocSinhController.cs
@@ -1,7 +1,10 @@
 ﻿using MongoDB.Driver;
-using System.Linq;
+using MongoDB.Bson;
 using System.Web.Mvc;
 using WebBus.Models;
+using System.Linq;
+using System.Collections.Generic;
+using static WebBus.Models.ViewModels;
 
 namespace WebBus.Areas.Admin.Controllers
 {
@@ -9,28 +12,108 @@ namespace WebBus.Areas.Admin.Controllers
     {
         private readonly MongoDBContext _context = new MongoDBContext();
 
-        #region Hiển thị danh sách học sinh
-        public ActionResult Index()
+        #region Hiển thị danh sách học sinh (với phân trang)
+        public ActionResult Index(int page = 1, int pageSize = 5)
         {
-            var hocSinh = _context.HocSinh.Find(_ => true).ToList();
-            return View(hocSinh);
+            var totalRecords = _context.HocSinh.Find(_ => true).CountDocuments();
+            var hocSinhList = _context.HocSinh
+                .Find(_ => true)
+                .Skip((page - 1) * pageSize)
+                .Limit(pageSize)
+                .ToList();
+
+            // Join với TuyenDuong để lấy TenTuyen
+            var tuyenDuongList = _context.TuyenDuong.Find(_ => true).ToList();
+            var hocSinhViewModel = hocSinhList.Select(h => new HocSinhViewModel
+            {
+                Id = h.Id,
+                userId = h.userId,
+                hoTen = h.hoTen,
+                lop = h.lop,
+                tuyenDuongId = h.tuyenDuongId,
+                tenTuyenDuong = tuyenDuongList.FirstOrDefault(t => t.Id == new ObjectId(h.tuyenDuongId))?.tenTuyen ?? "Không xác định"
+            }).ToList();
+
+            ViewBag.TotalRecords = totalRecords;
+            ViewBag.PageSize = pageSize;
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = (int)System.Math.Ceiling((double)totalRecords / pageSize);
+
+            return View(hocSinhViewModel);
         }
         #endregion
 
         #region Thêm mới học sinh
+        public ActionResult Create()
+        {
+            ViewBag.TuyenDuongList = _context.TuyenDuong.Find(_ => true).ToList();
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Create(WebBus.Models.HocSinh hocSinh)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.HocSinh.InsertOne(hocSinh);
+                return RedirectToAction("Index");
+            }
+            ViewBag.TuyenDuongList = _context.TuyenDuong.Find(_ => true).ToList();
+            return View(hocSinh);
+        }
         #endregion
 
         #region Cập nhật học sinh
+        public ActionResult Edit(string id)
+        {
+            var hocSinh = _context.HocSinh.Find(h => h.Id == new ObjectId(id)).FirstOrDefault();
+            if (hocSinh == null) return HttpNotFound();
+            ViewBag.TuyenDuongList = _context.TuyenDuong.Find(_ => true).ToList();
+            return View(hocSinh);
+        }
 
+        [HttpPost]
+        public ActionResult Edit(WebBus.Models.HocSinh hocSinh)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.HocSinh.ReplaceOne(h => h.Id == hocSinh.Id, hocSinh);
+                return RedirectToAction("Index");
+            }
+            ViewBag.TuyenDuongList = _context.TuyenDuong.Find(_ => true).ToList();
+            return View(hocSinh);
+        }
         #endregion
 
-        #region Xem chi tiết học sinh
+        #region Xem chi tiết học sinh (hiển thị tên tuyến đường)
+        public ActionResult Details(string id)
+        {
+            var hocSinh = _context.HocSinh.Find(h => h.Id == new ObjectId(id)).FirstOrDefault();
+            if (hocSinh == null) return HttpNotFound();
 
+            var tuyenDuong = _context.TuyenDuong
+                .Find(t => t.Id == new ObjectId(hocSinh.tuyenDuongId))
+                .FirstOrDefault();
+
+            ViewBag.TenTuyenDuong = tuyenDuong?.tenTuyen ?? "Không xác định";
+            return View(hocSinh);
+        }
         #endregion
 
         #region Xóa học sinh
+        public ActionResult Delete(string id)
+        {
+            var hocSinh = _context.HocSinh.Find(h => h.Id == new ObjectId(id)).FirstOrDefault();
+            if (hocSinh == null) return HttpNotFound();
+            return View(hocSinh);
+        }
 
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            _context.HocSinh.DeleteOne(h => h.Id == new ObjectId(id));
+            return RedirectToAction("Index");
+        }
         #endregion
     }
 }
---
+++ DichVuBus/WebBus/Models/ViewModels.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBus.Models
+{
+    public class ViewModels
+    {
+        public class HocSinhViewModel
+        {
+            public ObjectId Id { get; set; }
+            public string userId { get; set; }
+            public string hoTen { get; set; }
+            public string lop { get; set; }
+            public string tuyenDuongId { get; set; }
+            public string tenTuyenDuong { get; set; }
+        }
+    }
+}
