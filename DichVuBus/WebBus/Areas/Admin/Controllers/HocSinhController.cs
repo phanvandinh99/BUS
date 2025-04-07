27d844a90a4f0e11222e926f284516c09f0732a9
Phan Dinh <81685801+phanvandinh99@users.noreply.github.com>
2025-04-07 21:44:02 +0700

    Update function admin

---
+++ DichVuBus/WebBus/Areas/Admin/Controllers/HocSinhController.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Driver;
+using System.Linq;
+using System.Web.Mvc;
+using WebBus.Models;
+
+namespace WebBus.Areas.Admin.Controllers
+{
+    public class HocSinhController : Controller
+    {
+        private readonly MongoDBContext _context = new MongoDBContext();
+
+        #region Hiển thị danh sách học sinh
+        public ActionResult Index()
+        {
+            var hocSinh = _context.HocSinh.Find(_ => true).ToList();
+            return View(hocSinh);
+        }
+        #endregion
+
+        #region Thêm mới học sinh
+
+        #endregion
+
+        #region Cập nhật học sinh
+
+        #endregion
+
+        #region Xem chi tiết học sinh
+
+        #endregion
+
+        #region Xóa học sinh
+
+        #endregion
+    }
+}
--- DichVuBus/WebBus/Areas/Admin/Controllers/HomeController.cs
+++ DichVuBus/WebBus/Areas/Admin/Controllers/HomeController.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using WebBus.Models;
 
 namespace WebBus.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MongoDBContext _context = new MongoDBContext();
+
         // GET: Admin/Home
         public ActionResult Index()
         {
